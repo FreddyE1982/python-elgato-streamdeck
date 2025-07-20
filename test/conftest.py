a52da01ad3e6e931152b6b38f9111d7d6cb27ed3
FreddyE1982 <frederik.egger@gmail.com>
2025-07-20 21:47:31 +0200

    Add pytest instructions

---
+++ test/conftest.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import pytest
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../src'))
+
+from StreamDeck.DeviceManager import DeviceManager
+
+@pytest.fixture(scope="module")
+def deck():
+    manager = DeviceManager(transport="dummy")
+    decks = manager.enumerate()
+    if not decks:
+        pytest.skip("No dummy StreamDecks available")
+    return decks[0]
